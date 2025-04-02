83d238c93c0db99e582b3e5d53d6991ce9fb0a89
Thayná Zacharias <thayna.zacharias@hacklab.com.br>
2025-04-02 09:27:38 -0300

    altera nome do tema

--- dev-scripts/zip.sh
+++ dev-scripts/zip.sh
@@ -2,5 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CDIR=$( pwd )
 cd $DIR/../themes
-rm -f ../zips/hacklab-theme.zip
-zip -r ../zips/hacklab-theme.zip hacklab-theme -x "hacklab-theme/node_modules/*"
+rm -f ../zips/amazon-underworld.zip
+zip -r ../zips/amazon-underworld.zip amazon-underworld -x "amazon-underworld/node_modules/*"
