712e264b8d49e23919f8674724078cf2406a200a
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-06-06 17:35:06 -0300

    Adjust page-map template

---
+++ themes/amazon-underworld/page-map.php
@@ -0,0 +1,9 @@
+<?php
+get_header();
+?>
+    <div class="container">
+        <div class="post-content content content--normal">
+            <?php the_content() ?>
+        </div>
+    </div>
+<?php get_footer();
