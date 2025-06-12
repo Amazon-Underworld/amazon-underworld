42781bf9399e10630b45aefb8034ea602419806d
Julia <julia.maria@hacklab.com.br>
2025-06-12 15:48:14 -0300

    Starts single journalism

--- themes/amazon-underworld/single.php
+++ themes/amazon-underworld/single.php
@@ -5,37 +5,61 @@
 
 get_header();
 the_post();
-$category = get_the_category();
+
+$post_id = get_the_ID();
+$categories = get_the_category();
 $excerpt = !empty( $post->post_excerpt ) ? wp_kses_post( $post->post_excerpt ) : '';
+$terms = get_the_terms($post_id, 'category');
+$intro = pods_field('intro');
+
+if(has_term('opinion', 'category', $post_id)){ ?>
+    <header class="post-header post-header__opinion container">
+        <?php get_template_part('template-parts/title/post-header'); ?>
+    </header>
+<?php }
+
+elseif(has_term('article', 'category', $post_id)){ ?>
+    <header class="post-header post-header__article container">
+        <?php get_template_part('template-parts/title/post-header'); ?>
+    </header>
+<?php }
+
+elseif(has_term('chronicle', 'category', $post_id)){ ?>
+    <header class="post-header post-header__chronicle container">
+        <?php get_template_part('template-parts/title/post-header'); ?>
+    </header>
+<?php }
+
+else{ ?>
+    <header class="post-header container">
+        <?php get_template_part('template-parts/title/post-header'); ?>
+    </header>
+<?php }
+echo do_shortcode('[main-header]')
 ?>
 
-<header class="post-header container">
-    <div class="post-header__featured-image">
-        <?= get_the_post_thumbnail(null, 'post-thumbnail',['class'=>'post-header__featured-image']); ?>
-    </div>
-
-    <div class="post-header__tags">
-
-    </div>
-
-    <h1 class="post-header__title"> <?php the_title(); ?> </h1>
-
-    <?php if( $excerpt ) : ?>
-        <p class="post-header__excerpt"><?= get_the_excerpt() ?></p>
-    <?php endif; ?>
-
-    <div class="post-header__meta container">
-        <p class="post-header__date"><?php _e('Published in ', 'hacklabr') ?><?= get_the_date() ?></p>
+<main class="post-grid">
+    <aside class="post-aside">
+        <div class="post-aside__meta container">
+            <p class="post-aside__date"><?= get_the_date('j F Y') ?></p>
+            <p class="post-aside__author"><?php _e('By ', 'hacklabr') ?><?= get_the_author() ?></p>
+        </div>
         <?php get_template_part('template-parts/share-links', null, ['link'=>get_the_permalink()]) ?>
+    </aside>
+    <div class="post-content">
+        <?php
+        if( $intro ){ ?>
+            <p class="post-content__intro"><?php echo $intro[0] ;?></p>
+        <?php
+        }
+        else{
+            if( $excerpt ) : ?>
+            <p class="post-content__intro"><?= get_the_excerpt() ?></p>
+        <?php endif;
+        } ?>
+        <div class="content content--normal">
+            <?php the_content() ?>
     </div>
-
-    <div class="post-header__author container">
-        <p class="post-header__author-name"><?php _e('Published by ', 'hacklabr') ?><?= get_the_author() ?></p>
-    </div>
-</header>
-
-<main class="post-content content content--normal">
-    <?php the_content() ?>
 </main>
 
 <?php get_template_part('template-parts/content/related-posts') ?>
---
+++ themes/amazon-underworld/template-parts/title/post-header.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template-part for displaying post-header
+ */
+
+$post_id = get_the_ID();
+$categories = get_the_category();
+$excerpt = !empty( $post->post_excerpt ) ? wp_kses_post( $post->post_excerpt ) : '';
+$terms = get_the_terms($post_id, 'category');
+?>
+
+
+    <div class="post-header__infos">
+        <div class="post-header__head">
+            <div class="post-header__tags">
+                <?php
+                if(!empty($categories)){
+                    foreach($categories as $category){
+                        if($category->slug != 'infoamazonia'){ ?>
+                        <a class="tag tag--<?= $category->slug ?>" href="<?= get_term_link($category, 'tag') ?>">
+                        <?= $category->name ?>
+                        </a>
+                    <?php }
+                    }
+                }
+                ?>
+            </div>
+
+            <h1 class="post-header__title"> <?php the_title(); ?> </h1>
+            <div class="post-header__author container">
+                <p class="post-header__author-name"><?php _e('By ', 'hacklabr') ?><?= get_the_author() ?></p>
+            </div>
+        </div>
+
+        <div class="post-header__meta container">
+
+            <p class="post-header__date"><?= get_the_date('j F Y') ?></p>
+        </div>
+    </div>
+    <div class="post-header__featured-image">
+        <?= get_the_post_thumbnail(null, 'post-thumbnail',['class'=>'post-header__featured-image']); ?>
+    </div>
