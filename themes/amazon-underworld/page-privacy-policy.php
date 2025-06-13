661f5a12fceae77052b51b81c01de5e3ecc6d1b1
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-06-13 10:19:05 -0300

    Add page privacy policy

---
+++ themes/amazon-underworld/page-privacy-policy.php
@@ -0,0 +1,12 @@
+<?php
+get_header();
+?>
+    <div class="privacy-policy container">
+        <div class="post-header post-header__separator">
+            <h1 class="post-header__title"><?php the_title() ?></h1>
+        </div>
+        <div class="post-content content content--normal">
+            <?php the_content() ?>
+        </div>
+    </div>
+<?php get_footer();
