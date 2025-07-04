5001821662de9ee976f87e6e7bd32b1c098dcd35
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-07-04 15:29:42 -0300

    Adjust strings in 404 page

--- themes/amazon-underworld/404.php
+++ themes/amazon-underworld/404.php
@@ -3,8 +3,8 @@
     <div class="error-404__message-box">
         <p class="error-label"><?php _e('ERROR 404', 'hacklabr') ?></p>
         <h1 class="page-not-found-title"><?php _e('PAGE NOT FOUND', 'hacklabr') ?></h1>
-        <p class="not-found-description"><?php _e('Lo sentimos, no hemos encontrado resultados que coincidan con tu criterio de búsqueda', 'hacklabr') ?></p>
-        <a href="<?= home_url() ?>" class="button"> <span><?php _e('IR A LA HOME', 'hacklabr') ?></span> </a>
+        <p class="not-found-description"><?php _e("Sorry, we haven't found any results that match your search criteria.", 'hacklabr') ?></p>
+        <a href="<?= home_url() ?>" class="button"> <span><?php _e('GO TO HOME', 'hacklabr') ?></span> </a>
     </div>
 </div>
 
