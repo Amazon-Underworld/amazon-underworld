ba4532c6471056a998911fdaa59e845278f09d88
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-07-04 12:05:21 -0300

    Adjust alignment of footer copyright

--- themes/amazon-underworld/footer.php
+++ themes/amazon-underworld/footer.php
@@ -6,7 +6,7 @@
             <?php dynamic_sidebar('footer_widgets') ?>
         </div>
         <div class="main-footer__copyright">
-            <span><?=_e('© 2025 AMAZON UNDERWORLD', 'hacklabr'); ?></span>
+            <span class="text-copyright"><?=_e('© 2025 AMAZON UNDERWORLD', 'hacklabr'); ?></span>
             <span class="font-copyright"><?=_e('WEB DESING BY RÉPLICA | OPEN SOURCE FONT BY ', 'hacklabr'); ?><a href="http://www.whence.com/kamal/" target="_blank">KAMAL MOSTAFA </a></span>
         </div>
     </footer>
