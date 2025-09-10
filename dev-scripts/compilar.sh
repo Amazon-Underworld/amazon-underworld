e4b24202bd420fa1886030a69a9398889e489b31
Julia <julia.maria@hacklab.com.br>
2025-09-10 10:06:50 -0300

    Fixes compilation of v2 blocks

--- dev-scripts/compilar.sh
+++ dev-scripts/compilar.sh
@@ -3,3 +3,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CDIR=$( pwd )
 cd $DIR/../themes/amazon-underworld
 docker run -it -v `pwd`:/compilar node:20 bash -c "cd compilar && npm install && npm run production"
+docker run -it -v `pwd`:/compilar node:20 bash -c "cd /compilar/themes/amazon-underworld/library/blocks/v2 && npm install && npm run build"
